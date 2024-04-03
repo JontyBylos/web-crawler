8ad34716e1afec16f91518d0f1e3c0034240ff87
Jonty <jontybylos@gmail.com>
2024-04-03 12:08:33 -0700

    Added getURLsfromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,7 @@
+const { JSDOM } = require('jsdom')
+
+
+
 function normalizeURL(url) {
     const myURL = new URL(url); 
     let domain = myURL.host
@@ -10,6 +14,39 @@ function normalizeURL(url) {
     return (`${domain}${path}`)
 }
 
+function getURLsFromHTML(htmlBody, baseURL){
+    const { JSDOM } = require('jsdom')
+    const dom = new JSDOM(htmlBody) 
+    const nodelist = dom.window.document.querySelectorAll('a')
+    const nodelistarr = Array.from(nodelist)
+    
+    
+    function urlIsRelative(url) {
+        return !(url.startsWith('http://') || url.startsWith('https://'));
+    }
+
+    function getHrefs(element){
+        let url = element.href; 
+        if (urlIsRelative(url)) {
+            url = baseURL + url;
+        }
+        if (url.charAt(url.length - 1) === "/") {
+            url = url.slice(0, -1);
+        }
+        return url;
+    }
+    
+    
+    console.log(nodelistarr.map(getHrefs))
+    
+    return nodelistarr.map(getHrefs)
+}
+
+const htmlBody = '<html><body><a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a><a href="/about/"><span>About</span></a></body></html>'
+getURLsFromHTML(htmlBody, 'https://blog.boot.dev')
+
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@jest/globals')
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
 
 test('normalizeURL slash', () => {
     expect(normalizeURL("https://blog.boot.dev/path/")).toBe("blog.boot.dev/path");
@@ -21,3 +21,14 @@ test('normalizeURL slash', () => {
   test('normalizeURL http', () => {
     expect(normalizeURL("http://blog.boot.dev/path/")).toBe("blog.boot.dev/path");
   });
+
+  test('getURLsFromHTML general', () => {
+    const htmlBody = '<html><body><a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a></body></html>'
+    expect(getURLsFromHTML(htmlBody, 'https://blog.boot.dev')).toEqual(["https://blog.boot.dev"]);
+  });
+
+  test('getURLsFromHTML multiple links, relative to absolute', () => {
+    const htmlBody = '<html><body><a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a><a href="/about/"><span>About</span></a></body></html>'
+    expect(getURLsFromHTML(htmlBody, 'https://blog.boot.dev')).toEqual(["https://blog.boot.dev", "https://blog.boot.dev/about"]);
+  });
+
